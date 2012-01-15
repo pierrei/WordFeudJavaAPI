d4f36f12dfc78118422489dcc097f1a75e3569a4
Pierre Ingmansson <github@alias.mrpi.nu>
2012-01-15 14:47:48 +0100

    Refactored the WordFeudClient and split it in two parts (created the new WordFeudHttpCommunicator)

--- src/main/java/nu/mrpi/wordfeudapi/WordFeudClient.java
+++ src/main/java/nu/mrpi/wordfeudapi/WordFeudClient.java
@@ -2,59 +2,30 @@ package nu.mrpi.wordfeudapi;
 
 import nu.mrpi.util.SHA1;
 import nu.mrpi.wordfeudapi.domain.*;
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.params.ConnRoutePNames;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 import sun.misc.BASE64Encoder;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static nu.mrpi.util.MathUtil.random;
 
 /**
  * @author Pierre Ingmansson
  */
 public class WordFeudClient {
 
-    public static final String CONTENT_TYPE_JSON = "application/json";
-    private String sessionId;
-
     private User loggedInUser = null;
-    private final HttpClient httpClient;
-    private static final Pattern SESSION_ID_COOKIE_PATTERN = Pattern.compile("sessionid=(.*?);");
+    private WordFeudHttpCommunicator wordFeudHttpCommunicator;
 
-    public WordFeudClient() {
-        httpClient = createHttpClient();
+    public WordFeudClient(WordFeudHttpCommunicator wordFeudHttpCommunicator) {
+        this.wordFeudHttpCommunicator = wordFeudHttpCommunicator;
     }
 
-    private HttpClient createHttpClient() {
-        final HttpClient httpClient = new DefaultHttpClient();
-
-        final String proxyHost = System.getProperty("proxy.host");
-        final String proxyPort = System.getProperty("proxy.port");
-        if (proxyHost != null) {
-            final HttpHost proxy = new HttpHost(proxyHost, Integer.parseInt(proxyPort));
-            httpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
-        }
-
-        return httpClient;
+    public WordFeudClient() {
+        this(new WordFeudHttpCommunicator());
     }
 
     public void useSessionId(final String sessionId) {
-        this.sessionId = sessionId;
+        wordFeudHttpCommunicator.setSessionId(sessionId);
         this.loggedInUser = new User();
         loggedInUser.setSessionId(sessionId);
     }
@@ -70,7 +41,7 @@ public class WordFeudClient {
 
         try {
             loggedInUser = User.fromJson(json.getString("content"));
-            loggedInUser.setSessionId(sessionId);
+            loggedInUser.setSessionId(wordFeudHttpCommunicator.getSessionId());
 
             return loggedInUser;
         } catch (JSONException e) {
@@ -387,79 +358,11 @@ public class WordFeudClient {
     }
 
     private JSONObject callAPI(final String path) {
-        return callAPI(path, "");
+        return wordFeudHttpCommunicator.call(path);
     }
 
     private JSONObject callAPI(final String path, final String data) {
-        try {
-            final HttpPost post = createPost(path, data);
-
-            if (sessionId != null) {
-                post.addHeader("Cookie", "sessiond=" + sessionId);
-            }
-
-            final HttpResponse response = httpClient.execute(post);
-
-            if (response.getStatusLine().getStatusCode() == 200) {
-                return handleResponse(response);
-            } else {
-                EntityUtils.consume(response.getEntity());
-                throw new WordFeudException("Got unexpected HTTP " + response.getStatusLine().getStatusCode() + ": " + response.toString());
-            }
-
-        } catch (IOException e) {
-            throw new RuntimeException("Error when contacting WordFeud API", e);
-        } catch (JSONException e) {
-            throw new RuntimeException("Could not parse JSON", e);
-        } finally {
-            httpClient.getConnectionManager().closeExpiredConnections();
-        }
-    }
-
-    private JSONObject handleResponse(final HttpResponse response) throws IOException, JSONException {
-        checkCookieHeader(response);
-
-        final JSONObject jsonObject = extractJsonFromResponse(response);
-
-        if (!"success".equals(jsonObject.getString("status"))) {
-            throw new WordFeudException("Error when calling API: " + jsonObject.getJSONObject("content").getString("type"));
-        }
-
-        return jsonObject;
-    }
-
-    private JSONObject extractJsonFromResponse(final HttpResponse response) throws IOException, JSONException {
-        final String responseString = EntityUtils.toString(response.getEntity());
-        return new JSONObject(responseString);
-    }
-
-    private void checkCookieHeader(final HttpResponse response) {
-        final Header[] cookies = response.getHeaders("Set-Cookie");
-        if (cookies != null && cookies.length > 0) {
-            sessionId = extractSessionIdFromCookie(cookies[0]);
-        }
-    }
-
-    private String extractSessionIdFromCookie(final Header cookie) {
-        final String cookieValue = cookie.getValue();
-        final Matcher matcher = SESSION_ID_COOKIE_PATTERN.matcher(cookieValue);
-        if (matcher.find()) {
-            return matcher.group(1);
-        }
-        return null;
-    }
-
-    private HttpPost createPost(final String path, final String data) throws UnsupportedEncodingException {
-        final HttpPost post = new HttpPost("http://" + calculateHostName() + "/wf" + path);
-        post.addHeader("Content-Type", CONTENT_TYPE_JSON);
-        post.addHeader("Accept", CONTENT_TYPE_JSON);
-        final HttpEntity entity = new StringEntity(data, "UTF-8");
-        post.setEntity(entity);
-        return post;
-    }
-
-    private String calculateHostName() {
-        return "game0" + random(1, 7) + ".wordfeud.com";
+        return wordFeudHttpCommunicator.call(path, data);
     }
 
     private String toJSON(final HashMap<String, ?> parameters) {
---
+++ src/main/java/nu/mrpi/wordfeudapi/WordFeudHttpCommunicator.java
@@ -0,0 +1,144 @@
+package nu.mrpi.wordfeudapi;
+
+import org.apache.http.*;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static nu.mrpi.util.MathUtil.random;
+
+/**
+ * @author Pierre Ingmansson
+ */
+public class WordFeudHttpCommunicator {
+    public static final String CONTENT_TYPE_JSON = "application/json";
+
+    public static final String SESSION_COOKIE_NAME = "sessionid";
+
+    private static final Pattern SESSION_ID_COOKIE_PATTERN = Pattern.compile(SESSION_COOKIE_NAME + "=(.*?);");
+
+    private final HttpClient httpClient;
+
+    private String sessionId;
+
+    public WordFeudHttpCommunicator() {
+        httpClient = createHttpClient();
+    }
+
+    private HttpClient createHttpClient() {
+        SchemeRegistry schemeRegistry = new SchemeRegistry();
+        schemeRegistry.register(
+                new Scheme("http", 80, PlainSocketFactory.getSocketFactory()));
+
+        ClientConnectionManager cm = new ThreadSafeClientConnManager(schemeRegistry);
+        HttpClient httpClient = new DefaultHttpClient(cm);
+
+        final String proxyHost = System.getProperty("proxy.host");
+        final String proxyPort = System.getProperty("proxy.port");
+        if (proxyHost != null) {
+            final HttpHost proxy = new HttpHost(proxyHost, Integer.parseInt(proxyPort));
+            httpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+        }
+
+        return httpClient;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public JSONObject call(final String path) {
+        return call(path, "");
+    }
+
+    public  JSONObject call(final String path, final String data) {
+        try {
+            final HttpPost post = createPost(path, data);
+
+            if (sessionId != null) {
+                post.addHeader("Cookie", SESSION_COOKIE_NAME + "=" + sessionId);
+            }
+
+            final HttpResponse response = httpClient.execute(post);
+
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                return handleResponse(response);
+            } else {
+                EntityUtils.consume(response.getEntity());
+                throw new WordFeudException("Got unexpected HTTP " + response.getStatusLine().getStatusCode() + ": " + response.toString());
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error when contacting WordFeud API", e);
+        } catch (JSONException e) {
+            throw new RuntimeException("Could not parse JSON", e);
+        } finally {
+            httpClient.getConnectionManager().closeExpiredConnections();
+        }
+    }
+
+    private JSONObject handleResponse(final HttpResponse response) throws IOException, JSONException {
+        checkCookieHeader(response);
+
+        final JSONObject jsonObject = extractJsonFromResponse(response);
+
+        if (!"success".equals(jsonObject.getString("status"))) {
+            throw new WordFeudException("Error when calling API: " + jsonObject.getJSONObject("content").getString("type"));
+        }
+
+        return jsonObject;
+    }
+
+    private JSONObject extractJsonFromResponse(final HttpResponse response) throws IOException, JSONException {
+        final String responseString = EntityUtils.toString(response.getEntity());
+        return new JSONObject(responseString);
+    }
+
+    private void checkCookieHeader(final HttpResponse response) {
+        final Header[] cookies = response.getHeaders("Set-Cookie");
+        if (cookies != null && cookies.length > 0) {
+            sessionId = extractSessionIdFromCookie(cookies[0]);
+        }
+    }
+
+    private String extractSessionIdFromCookie(final Header cookie) {
+        final String cookieValue = cookie.getValue();
+        final Matcher matcher = SESSION_ID_COOKIE_PATTERN.matcher(cookieValue);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return null;
+    }
+
+    private HttpPost createPost(final String path, final String data) throws UnsupportedEncodingException {
+        final HttpPost post = new HttpPost("http://" + calculateHostName() + "/wf" + path);
+        post.addHeader("Content-Type", CONTENT_TYPE_JSON);
+        post.addHeader("Accept", CONTENT_TYPE_JSON);
+        final HttpEntity entity = new StringEntity(data, "UTF-8");
+        post.setEntity(entity);
+        return post;
+    }
+
+    private String calculateHostName() {
+        return "game0" + random(1, 7) + ".wordfeud.com";
+    }
+}
